52c0eed2821328a84d4ea0a282afd65c0a87b652
wakuike <shoma.aptx4869@gmail.com>
2020-02-04 04:42:17 +0900

    Initial Commit

---
+++ firebaseBeginner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  firebaseBeginner
+//
+//  Created by 池内将真 on 2020/02/04.
+//  Copyright © 2020 wakuike. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
